bfbd250f8a2411bf3db4fff4488d7139d1adee4c
billthomjordan <60418813+billthomjordan@users.noreply.github.com>
2020-01-29 12:22:14 +0700

    Create datarestoran.php

---
+++ datarestoran.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <script  src="https://kit.fontawesome.com/82983a7696.js" crossorigin="anonymous"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>DATA RESTORAN</title>
+<style type ="text/css";>
+.text{
+  margin-top: 100px;
+  font-size: 100px;
+}
+</style>
+  </head>
+  <body class="bg-dark" id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top">
+      <div class="container">
+  <a class="navbar-brand font-weight-bold text-white">JORDAN MAP GIS</a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link js-scroll-trigger text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="map.php">Map</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle js-scroll-trigger text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Data
+        </a>
+        <div class="dropdown-menu text-white" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="dataspbu.php">SPBU</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="datahotel.php">Hotel</a>
+          <a class="dropdown-item" href="datarestoran.php">Restoran</a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="row">
+          <div class="col">
+            <button type="button" class="btn btn-transparent text-white" data-toggle="modal" data-target="#contact">Contact</a></button>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="row">
+          <div class="col">
+            <button type="button" class="btn btn-transparent text-white" data-toggle="modal" data-target="#Modal">AboutUs</button>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</div>
+</nav>
+<div class="jumbotron">
+  <div class="container">
+<div class="modal" id="contact">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title font-weight-bold">CONTACT US</h2>
+        <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+      </div>
+      <div class="modal-body">
+      <ul>
+        <li><i class="fab fa-youtube text-danger">MICKEY GT</i></li>
+        <li><i class="fab fa-facebook text-primary">Billthom Jordan</i></li>
+        <li><i class="fab fa-instagram text-warning">billthomjordan</i></li>
+        <li><i class="fab fa-whatsapp text-success">0895613137056</i></li>
+      </ul>
+      <h4 class="font-weight-bold text-center">THANK YOU</h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal" id="Modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title">WELCOME TO JORDAN MAP</h2>
+        <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+      </div>
+      <div class="modal-body">
+        Terimakasih Sudah mengunjungi Website Kami
+        <p><h6 class="white">Copyright &copy; Billthom Jordan (BillthomJordan) 2020</h6><p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+  </div>
+</div>
+
+<h1 class="text text-center text-danger font-weight-bold"><u>MAINTENANCE</u></h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
